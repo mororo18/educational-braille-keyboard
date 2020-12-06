6c77354b8772517f3f50c57e5f76fc09c9e37d72
mororo18 <victormororo@gmail.com>
2020-12-06 20:02:28 -0300

    enable simulation

--- simulate.py
+++ simulate.py
@@ -8,26 +8,69 @@ monitor_CSS_SELECTOR = 'div.code_panel__serial__content__text.js-code_panel__ser
 code_button_XPATH = '/html/body/div[2]/div/div/div[4]/div/div/div/div[1]/div[3]/a'
 clear_button_CSS_SELECTOR = 'a.circ_btn.circ_btn--m.circ_btn--none_accent_accent.code_panel__serial__button.js-code_panel__serial__clear.js-analytics'
 
+punct = {
+    ',' : "comma",
+    ';' : "semicolon",
+    ':' : "colon",
+    '.' : "dot",
+    '?' : "question",
+    '!' : "exclamation",
+    '-' : "hyphen"
+}
+
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
 
+def what_mode():
+    mode_message = "[+] Current Input Mode:"
+    MODE_POS = 24
+
+    content = driver.find_element_by_css_selector(monitor_CSS_SELECTOR)
+    content = content.text
+    pos = content.rfind(mode_message)
+
+    if pos != -1:
+        mode = content[(MODE_POS+pos):]
+        
+        filename = "sounds/" + mode +".wav"
+        message = "\nOutputing \'" + mode + " Mode\' Sound..."
+        print(message)
+        playsound(filename)
+
+
+def symb_to_word(symb):
+
+    if ('A' <= symb and symb <= 'Z') or ('0' <= symb and symb <= '9') :
+        return str(symb)
+    
+    else:
+        return punct[symb]
+
+
 def get_char():
     char_message = "[+] Corresponding character:"
+    error_message = "[!] Error message:"
     CHAR_POS = 30
 
     content = driver.find_element_by_css_selector(monitor_CSS_SELECTOR)
     content = content.text
-    pos = content.rfind(char_message)
+    # in case of mode change
+    if content == "":
+        return
 
-    if(pos != -1):
-        char = content[pos + CHAR_POS]
-        filename = "sounds/" + str(char) + ".wav"
-        message = "\nOutputing \'" + str(char) + "\' Sound..."
+    pos_c = content.rfind(char_message)
+    pos_e = content.rfind(error_message)
+
+    if(pos_c != -1):
+        char = content[pos_c + CHAR_POS]
+        char = symb_to_word(char)
+        filename = "sounds/" + char + ".wav"
+        message = "\nOutputing \'" + char + "\' Sound..."
 
         print(message)
         playsound(filename)
 
-    else:
+    elif pos_e != -1:
         print("\nOutputing Error Sound..\n")
         playsound("sounds\error.wav")
 
@@ -41,6 +84,7 @@ def monitor_observer():
         if (content != ""):
             sleep(2)
 
+            what_mode()
             get_char()
 
             clear_button = driver.find_element_by_css_selector(clear_button_CSS_SELECTOR)
@@ -48,7 +92,7 @@ def monitor_observer():
 
 def start_simulation():
     
-    driver.get('https://www.tinkercad.com/things/6Vrjlavkf2A/viewel')
+    driver.get('https://www.tinkercad.com/things/ktPH2wSIrIG/viewel')
     sleep(6)
     
     code_button = driver.find_element_by_xpath(code_button_XPATH)
