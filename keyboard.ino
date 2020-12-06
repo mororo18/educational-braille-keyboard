506b09e1f35d296e9fead2b7cb09fc8124f39f82
mororo18 <victormororo@gmail.com>
2020-12-06 01:38:15 -0300

    add modes feature (+ numeric and ponctuation)

--- keyboard.ino
+++ keyboard.ino
@@ -1,11 +1,35 @@
+#define MODE_BTN     9
 #define CHECK        2          // pin of check button input
 #define LOWER_BUTTON 3          // braille button with the lowest pin number
 
 #define COLUMNS 7
-#define RAWS    26
+#define RAWS    33
+
+// modes search scope BEGIN
+#define ALPH_FRST_RW   0 
+#define ALPH_LST_RW    25
+
+#define NUM_FRST_RW    0
+#define NUM_LST_RW     9
+
+#define PONCT_FRST_RW 26
+#define PONCT_LST_RW  32
+//modes search scope END
 
 #define BRAILLE_BTNS 6
+#define MODES 3                // quantity of implemented modes
 
+/*
+   current keyboard mode, starting w alphabetic
+
+   MODES
+ */
+
+#define ALPHA 0
+#define NUM   1
+#define PONCT  2
+
+int mode = 0;
 
 // braille alphabet
 int alphabet[RAWS][COLUMNS] = {
@@ -34,11 +58,20 @@ int alphabet[RAWS][COLUMNS] = {
     {0, 1, 0, 1, 1, 1, 'W'},
     {1, 0, 1, 1, 0, 1, 'X'},
     {1, 0, 1, 1, 1, 1, 'Y'},
-    {1, 0, 1, 0, 1, 1, 'Z'}
+    {1, 0, 1, 0, 1, 1, 'Z'},
+    {0, 1, 0, 0, 0, 0, ','},
+    {0, 1, 1, 0, 0, 0, ';'},
+    {0, 1, 0, 0, 1, 0, ':'},
+    {0, 0, 1, 0, 0, 0, '.'},
+    {0, 1, 0, 0, 0, 1, '?'},
+    {0, 1, 1, 0, 1, 0, '!'},
+    {0, 0, 1, 0, 0, 1, ','}
 };
 
 int input[BRAILLE_BTNS] = {0, 0, 0, 0, 0, 0};
 
+char alphabet_to_num(char*);
+bool change_mode(bool);
 int keyboard_input(int);
 void find_character(int);
 
@@ -51,13 +84,18 @@ void setup(){
     pinMode(6, INPUT);  // 3rd BTN
     pinMode(7, INPUT);  // 4th BTN
     pinMode(8, INPUT);  // 5th BTN
+    pinMode(9, INPUT);  // Mode BTN
     Serial.begin(9600);
+
+    Serial.println("[+] Current Input Mode: Alphabetic");
 }
 
 void loop(){
     int check_button = digitalRead(CHECK);
+    bool mode_button = digitalRead(MODE_BTN);
     int input_state = 0;
-    
+
+    change_mode(mode_button);
     input_state = keyboard_input(check_button);
     find_character(input_state);     
 }
@@ -66,7 +104,7 @@ int keyboard_input(int check_button){
 
     if(check_button){
         delay(500);
-        
+
         Serial.print("[+] Keyboard Input:\t\t");
         for(int i = 0; i <  BRAILLE_BTNS; i++){
 
@@ -78,39 +116,101 @@ int keyboard_input(int check_button){
 
             Serial.print(input[i]);
         }
-    
+
         Serial.println(';');
-        
+
         return 1;
     }
 }
 
 void find_character(int state){
     int flag;
+    // search scope
+    int first_raw, last_raw;
     char chrt;
-    
+
+    switch(mode){
+        case ALPHA:
+            first_raw = ALPH_FRST_RW;
+            last_raw = ALPH_LST_RW;
+            break;
+        case NUM:
+            first_raw = NUM_FRST_RW;
+            last_raw = NUM_LST_RW;
+            break;
+        case PONCT:
+            first_raw = PONCT_FRST_RW;
+            last_raw = PONCT_LST_RW;
+        default:
+            break;
+    }
+
     if(state){
-        for(int i = 0; i < RAWS; i++){
-                flag = 1;
-        
+        for(int i = first_raw; i <= last_raw; i++){
+            flag = 1;
+
             for(int j = 0; j < COLUMNS - 1; j++){
                 if(alphabet[i][j] != input[j]){
                     flag = 0;
                     break;
                 }
             }
-        
+
             if(flag){
                 // matched character
                 chrt = alphabet[i][COLUMNS - 1];
-                Serial.println("[+] Corresponding character:\t'" + (String)chrt + "';");                
+
+                // if numeric mode
+                if(mode == NUM)
+                    alphabet_to_num(&chrt);
+
+                // monitor print
+                Serial.println("[+] Corresponding character:\t'"+(String)chrt+"';");                
                 Serial.println("[+] Outputing character sound...\n");
                 return;
             }
         }
-        //monitor error messages 
+
         Serial.println("[!] Error message:\t\tCharacter not found;\n"
-                       "[+] Outputing Sound...\t\t\"Hmm, ainda nao conheco essa letrinha.\";\n");
-    }   
+                "[+] Outputing Sound...\t\t\"Hmm, ainda nao conheco essa letrinha.\";\n");
+    }       
 }
 
+bool change_mode(bool state){
+
+    if(state){
+        delay(150);
+        mode++;
+
+        if(mode >= MODES)
+            mode = 0;
+
+        Serial.print("[+] Current Input Mode: ");
+        switch(mode){
+            case ALPHA:
+                Serial.println("Alphabetic");
+                break;
+            case NUM:
+                Serial.println("Numeric");
+                break;
+            case PONCT:
+                Serial.println("Ponctuation");
+            default:
+                break;
+        }
+
+    }
+}
+
+char alphabet_to_num(char *letter){
+    char *num;
+
+    num = letter;
+
+    *num -= 'A';
+    *num += '0';
+    *num += 1;
+
+    if(*num > '9')
+        *num = '0';
+}
